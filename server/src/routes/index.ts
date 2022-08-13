665cf9e84e08e924bbdd22c66bc5c8cdece1917c
Nazmul Haque <58416190+nazmul53p@users.noreply.github.com>
2022-08-14 00:34:11 +0600

    Delete server directory

--- server/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { RouterT } from '../types';
-import ProfileRoute from './ProfileRoute';
-import UserRoute from './UserRoute';
-import SkillRoute from './SkillRoute';
-
-const routers: RouterT[] = [UserRoute, ProfileRoute, SkillRoute];
-
-export default routers;
--- server/src/types/index.ts
+++
@@ -1,5 +0,0 @@
-import ProfileRoute from 'routes/ProfileRoute';
-import userRoute from '../routes/UserRoute';
-import SkillRoute from '../routes/SkillRoute';
-
-export type RouterT = typeof userRoute | typeof ProfileRoute | typeof SkillRoute;
