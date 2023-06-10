945ff9892b9c8da5934f400df26311d1b105ba7c
nazmul53p <nazmul20018s@gmail.com>
2023-06-10 19:24:56 +0600

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,7 +44,7 @@ const profile = {
 		address: 'Meherpur, Bangladesh',
 		email: 'nazmul2018s@gmail.com',
 		phone: '+8801917003822',
-		cvLink: 'https://drive.google.com/file/d/1zSMGmnVXdzHUE6P---JDSgML6vBJiPGS/view?usp=sharing'
+		cvLink: 'https://drive.google.com/file/d/18ss0OZV38sLY_R0fKv-eg78gxxFINQxd/view?usp=sharing'
 	},
 	skills: {
 		title: 'Skill',
